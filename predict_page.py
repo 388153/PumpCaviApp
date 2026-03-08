a8e551e98056c2b493bdfa70e1319dbc5758c915
388153 <patiparn@eng.buu.ac.th>
2026-03-08 17:36:34 +0700

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -13,7 +13,7 @@ st.subheader("Prediction of Cavitation in Centrifugal Pump")
 
 st.warning(":orange[ตัวอย่าง ค่าการสั่นสะเทือนในแนวแกน x,y,z และค่าเป้าหมาย ที่ใช้ในการสร้าง classifier model] ")
 vibra_input = pd.read_csv('Cavi.csv')
-st.slidebar("กรุณาเลือกข้อมูลข้างล่างต่อไปนี้")
+st.sidebar("กรุณาเลือกข้อมูลข้างล่างต่อไปนี้")
 #st.write(vibra_input.sample(3))
 
 #X = vibra_input[['Acceleration x (m/s^2)', 'Acceleration y (m/s^2)','Acceleration z (m/s^2)']]
